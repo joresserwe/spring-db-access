7fc00efe0ed2be36b6b838f42fd6ebe59140b67b
Eunbin <kartrider_12@naver.com>
2022-07-03 04:00:22 +0900

    add test to use embedded db

--- src/main/kotlin/me/dataaccess/DataAccessApplication.kt
+++ src/main/kotlin/me/dataaccess/DataAccessApplication.kt
@@ -2,6 +2,7 @@ package me.dataaccess
 
 import me.dataaccess.config.JdbcTemplateV3Config
 import me.dataaccess.repository.ItemRepository
+import mu.KotlinLogging
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
@@ -14,9 +15,21 @@ import org.springframework.context.annotation.Profile
 @Import(JdbcTemplateV3Config::class)
 @SpringBootApplication(scanBasePackages = ["me.dataaccess.web"])
 class DataAccessApplication {
+
+    private val log = KotlinLogging.logger {}
+
     @Bean
     @Profile("local")
     fun testDataInit(itemRepository: ItemRepository) = TestDataInit(itemRepository)
+
+    /* @Bean
+     @Profile("test")
+     fun dataSource() = DriverManagerDataSource().apply {
+         setDriverClassName("org.h2.Driver")
+         url = "jdbc:h2:mem:db;DB_CLOSE_DELAY=-1" // Database Connection이 모두 끊어질 경우 DB도 종료된다. 방지
+         username = "sa"
+         password = ""
+     }.also { log.info { "Memory Database 초기화!!" } }*/
 }
 
 fun main(args: Array<String>) {
--- src/test/kotlin/me/dataaccess/domain/ItemRepositoryTest.kt
+++ src/test/kotlin/me/dataaccess/domain/ItemRepositoryTest.kt
@@ -3,24 +3,18 @@ package me.dataaccess.domain
 import me.dataaccess.repository.ItemRepository
 import me.dataaccess.repository.ItemSearchCond
 import me.dataaccess.repository.ItemUpdateDto
-import me.dataaccess.repository.memory.MemoryItemRepository
 import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.transaction.annotation.Transactional
 
+@Transactional
 @SpringBootTest
 internal class ItemRepositoryTest {
 
     @Autowired
     private lateinit var itemRepository: ItemRepository
-
-    @AfterEach
-    fun afterEach() {
-        //MemoryItemRepository 의 경우 제한적으로 사용
-        (itemRepository as? MemoryItemRepository)?.clearStore()
-    }
 
     @Test
     fun save() {
@@ -28,10 +22,10 @@ internal class ItemRepositoryTest {
         val item = Item("itemA", 10000, 10)
 
         //when
-        val savedItem: Item = itemRepository.save(item)
+        val savedItem = itemRepository.save(item)
 
         //then
-        val findItem: Item = itemRepository.findById(item.id)
+        val findItem = itemRepository.findById(item.id)
         Assertions.assertThat(findItem).isEqualTo(savedItem)
     }
 
@@ -47,7 +41,7 @@ internal class ItemRepositoryTest {
         itemRepository.update(itemId, updateParam)
 
         //then
-        val (itemName, price, quantity) = itemRepository.findById(itemId)
+        val (itemName, price, quantity) = itemRepository.findById(itemId)!!
         Assertions.assertThat(itemName).isEqualTo(updateParam.itemName)
         Assertions.assertThat(price).isEqualTo(updateParam.price)
         Assertions.assertThat(quantity).isEqualTo(updateParam.quantity)
